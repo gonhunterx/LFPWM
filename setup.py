e09b7ee8af3f8ed51991d288c5e12941da663427
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-11-08 07:02:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.13'
 DESCRIPTION = 'LamaForge Encrypted Password Manager'
 LONG_DESCRIPTION = 'A package that allows you to store encrypted data locally.'
