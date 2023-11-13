2954319923513e96de8eb3cf3f377981f5eeb8f1
Jadon <47489511+gonhunterx@users.noreply.github.com>
2023-11-12 20:55:20 -0500

    update

--- LFPWM/LFPWM_main.py
+++ LFPWM/LFPWM_main.py
@@ -1,77 +1,9 @@
-from LFPWM.sql_db import create_connection, create_database, create_users_data_table
-from LFPWM.users import User
-from LFPWM.home import home_page
-
-conn, c = create_connection()
-
-
-def login():
-    print("Please log in 😊")
-    username = input("Username: ")
-    password = input("Password: ")
-    # use the static method from User class to verify
-    user_id = User.login(username, password)
-
-    if user_id:
-        print("Login successful! User ID:", user_id)
-        user = User(username, password)
-    else:
-        print("Invalid username or password. Please try again. ")
-        main()
-    home_page(user)
-
-
-def register():
-    print("Please create an account 😊")
-    username = input("Username: ")
-    c.execute("SELECT user_id FROM Users WHERE username = ?", (username,))
-    existing_user = c.fetchone()
-    if existing_user:
-        print("This username is already in use. Please use another.")
-    else:
-        password = input("Password: ")
-        c.execute(
-            "INSERT INTO Users(username, password) VALUES (?, ?)", (username, password)
-        )
-        conn.commit()
-        print("Account created successfully!")
-
-
-def main_menu():
-    print(
-        """
-1. Login
-2. Register
-3. Exit
-          """
-    )
-    choice = input("Input: ")
-    return choice
+from sql_db import create_tables
 
 
 def main():
-    try:
-        print("=======================")
-        print("Welcome to LamaForge PW Manager")
-        create_database()
-        while True:
-            login_menu_choice = main_menu()
-            if login_menu_choice == "1":
-                login()
-            elif login_menu_choice == "2":
-                register()
-            elif login_menu_choice == "3":
-                conn.close()  # Close the connection before exiting
-                break
-            else:
-                print("Invalid input. Please input 1, 2, or 3.")
-    finally:
-        conn.close()
+    create_tables()
 
 
 if __name__ == "__main__":
-    create_users_data_table()
     main()
-
-# change the main statement into another user level function that they have access to throught he package
-# and the first thing they run is main and others will be able to run it.
